fa556a7412eb9f4de24de3ba0e76cb2cb8080995
AhmadMajar98 <ahmad.majar98@gmail.com>
2024-11-14 18:13:15 +0300

    create UI chat

--- front-end/src/components/work/chat.js
+++ front-end/src/components/work/chat.js
@@ -3,66 +3,39 @@ import Send from '../../imgs/send.svg'
 
 function Chat() {
   return (
-    <div className="max-w-full h-screen mx-auto bg-white dark:bg-[#1f2937] flex flex-col ">
+    <div className="max-w-full h-screen mx-auto bg-[#E7EDF9] dark:bg-[#010B13] flex flex-col ">
 
       <div className="relative h-48 overflow-hidden">
         <img src="" alt="Background Image" className="object-cover w-full h-full" />
       </div>
 
-      <div className="py-4 space-y-4 flex-grow overflow-auto ">
-        <div className="flex space-x-3 items-start">
-          <div className="w-10 h-10 rounded-full bg-gray-300"></div>
-          <div>
-            <div className="mt-1 bg-blue-500 text-white p-2 rounded-lg shadow-md max-w-xs">
-              <p className="text-xs font-medium text-gray-600 dark:text-blue-300">Member 1</p>
-
-              <p>Hello Bossy</p>
+      <div className="py-4 space-y-4 flex-grow overflow-auto px-3">
+        <div className="flex space-x-3 items-start h-auto">
+          <div className="w-10 h-10 rounded-full bg-gray-300" ></div>
+          <div className="w-full">
+            <div className="mt-1 bg-[#7C9ED9] text-white p-2 rounded-lg shadow-md max-w-xs">
+              <p className="text-xs font-medium text-gray-600 dark:text-blue-300">Manager</p>
+              <p className="whitespace-normal break-words">
+                t! It will automatically adjust the height ocdvcxvcxvcxvcxvcxvcxvcxvcv zxczxckjckjzkxmckzxkcmkzxnkcnkznxkcthe box.
+              </p>
             </div>
           </div>
         </div>
 
         <div className="flex justify-end space-x-3 items-start">
           <div>
-            <p className="text-xs font-medium text-gray-500 dark:text-green-300">Manager</p>
-            <div className="mt-1 bg-gray-100 dark:bg-[#374151] text-gray-800 dark:text-gray-200 p-2 rounded-lg shadow-md max-w-xs">
-            </div>
-          </div>
-          <div className="w-10 h-10 rounded-full bg-gray-300"></div>
-        </div>
+            <div className="mt-1 bg-gray-100 dark:bg-[#F4F4F4] text-gray-800 dark:text-black p-2 rounded-lg shadow-md max-w-xs">
+              <p className="text-xs font-medium text-[gray-600] dark:text-[#7C9ED9]">Manager</p>
 
-        <div className="flex space-x-3 items-start">
-          <div className="w-10 h-10 rounded-full bg-gray-300"></div>
-          <div>
-            <p className="text-xs font-medium text-gray-600 dark:text-blue-300">Member 1</p>
-            <div className="mt-1 bg-blue-500 text-white p-2 rounded-lg shadow-md max-w-xs">
-              Hello Boss
-            </div>
-          </div>
-        </div>
-
-        <div className="flex space-x-3 items-start">
-          <div className="w-10 h-10 rounded-full bg-gray-300"></div>
-          <div>
-            <p className="text-xs font-medium text-gray-600 dark:text-blue-300">Member 2</p>
-            <div className="mt-1 bg-blue-500 text-white p-2 rounded-lg shadow-md max-w-xs">
-              Hello Boss
-            </div>
-          </div>
-        </div>
-
-        <div className="flex justify-end space-x-3 items-start">
-          <div>
-            <p className="text-xs font-medium text-gray-500 dark:text-green-300">Manager</p>
-            <div className="mt-1 bg-gray-100 dark:bg-[#374151] text-gray-800 dark:text-gray-200 p-2 rounded-lg shadow-md max-w-xs">
-              Hello, how is your task?
+              <p className='whitespace-normal break-words'>Hello ,How are your Tasks</p>
             </div>
           </div>
           <div className="w-10 h-10 rounded-full bg-gray-300"></div>
         </div>
       </div>
 
-      <div className="flex items-center m-4 bg-gray-100 dark:bg-[#374151] p-2 rounded-full shadow-md">
-        <input type="text" placeholder="Type a message..." className="flex-grow bg-transparent text-gray-800 dark:text-gray-200 focus:outline-none px-2" />
+      <div className="flex items-start m-4 bg-gray-100 dark:bg-[#374151] p-2 rounded-full shadow-md">
+        <input type="text" placeholder="Type a message..." className="flex-grow bg-transparent text-gray-800 dark:text-gray-200 focus:outline-none px-2 " />
         <button className="text-gray-500 dark:text-gray-400 px-2">
           <i className="fas fa-paperclip"></i>
         </button>
@@ -70,7 +43,7 @@ function Chat() {
           <i className="fas fa-microphone"></i>
         </button>
         <button className="text-blue-500 px-2">
-          Send
+          <img src={Send} />
         </button>
       </div>
     </div>
