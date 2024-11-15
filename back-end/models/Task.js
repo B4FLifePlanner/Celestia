0deb99fe3e9b5142d6388131d26f74e18d2d05cf
AhmadMajar98 <ahmad.majar98@gmail.com>
2024-11-15 17:43:38 +0300

    create task show api >> but need to test

--- back-end/models/Task.js
+++ back-end/models/Task.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema
 const taskSchema = new Schema({
     Title: { type: String, required: true},
     Info: { type: String, required: true},
-    AssignedTo: { type: String, required: true},
+    AssignedTo: [{ type: Schema.Types.ObjectId, ref: 'User', required: true}],
     Status: { type: String, enum: ['Pending','Waiting To Review', 'Rejected', 'Approved'], default: 'Pending', required: true},
     Deadline: { type: Date, required: true},
     created_at: { type: Date, default: Date.now}
--- back-end/routes/tasks.js
+++ back-end/routes/tasks.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Task = require('../models/Task')
+const checkUserRole = require('../middlewares/checkWorkTasks')
 
 
 const router = express.Router()
@@ -27,5 +28,20 @@ router.post('/Add-Task',  async (req, res) => {
         res.status(400).json({ error: error.message });
      }
 })
+
+router.get('/:userId', checkUserRole, async (req, res) => {
+    try {
+        const tasks = await Task.find(req.taskQuery);
+
+        if (tasks.length === 0) {
+            return res.status(404).json({ message: 'No tasks found' });
+        }
+
+        res.status(200).json(tasks);
+    } catch (error) {
+        console.error('Error fetching tasks:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
 
 module.exports = router
